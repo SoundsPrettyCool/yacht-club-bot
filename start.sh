c840b74897fa0157704ef2540ef37ab8ebfeb998
Carlos Garcia <carlosgarcia@Cassandras-MacBook-Air.local>
2024-12-07 19:09:03 -0500

    update docker-file

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Check if ENVIRONMENT is set to 'dev'
-if [ "$ENVIRONMENT" = "dev" ]; then
+if [ "$BOT_ENVIRONMENT" = "dev" ]; then
     # Load .env file if it exists
     if [ -f .env ]; then
         export $(grep -v '^#' .env | xargs)
     fi
 else
-    echo "Skipping .env file loading: ENVIRONMENT is not 'dev'."
+    echo "Skipping .env file loading: BOT_ENVIRONMENT is not 'dev'."
 fi
 
 # Run the application
